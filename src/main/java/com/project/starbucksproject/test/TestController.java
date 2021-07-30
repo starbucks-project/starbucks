fe0b5cf24e4f324b261e28d20fd6fca2147dd6ec
DESKTOP-AB63HLR\gahyun <hyunsybm@gmail.com>
2021-07-30 20:43:01 +0900

    가현 프로젝트

--- src/main/java/com/project/starbucksproject/test/TestController.java
+++ src/main/java/com/project/starbucksproject/test/TestController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TestController {
 
-    @GetMapping("/test") 
-    public String test(){
+    @GetMapping("/test")
+    public String test() {
         return "test/test";
+    }
+
+    @GetMapping("/store")
+    public String store() {
+        return "auth/store";
     }
 }
