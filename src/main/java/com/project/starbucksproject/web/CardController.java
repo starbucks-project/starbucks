bd143b1b0c586374200e26ef86bad44547fc1db4
DESKTOP-AB63HLR\gahyun <hyunsybm@gmail.com>
2021-08-09 22:48:18 +0900

    gh update

--- src/main/java/com/project/starbucksproject/web/CardController.java
+++ src/main/java/com/project/starbucksproject/web/CardController.java
@@ -12,7 +12,6 @@ import com.project.starbucksproject.domain.user.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -34,6 +33,11 @@ public class CardController {
       System.out.println("CardName:"+card.getCardName());
       System.out.println(card.getCardNum());
       System.out.println(card.getPin());
+
+      if(card.getCardName()==null){
+        List<Card> cardList=cardRepository.findAll();
+        //cardList.
+      }
       
       User principal=(User)session.getAttribute("principal");
       if(principal==null) {
@@ -74,17 +78,5 @@ public class CardController {
 
       return "user/inMyCard";
   }
-
-
-  @GetMapping("user/cardCharge")
-  public String userinfo(Model model) {
-    User principal=(User)session.getAttribute("principal");
-    int userid = principal.getId();
-    List<Card> cardsEntity = cardRepository.mfindByAlluserId(userid);
-    model.addAttribute("cardsEntity", cardsEntity);
-
-    return "/user/cardCharge"; 
-  }
-
 
 }
